58a663993e4ac633969481f22bfaa8e101b1ef1e
iuliagheju <162321949+iuliagheju@users.noreply.github.com>
2024-11-07 22:21:48 +0200

    Resolved merge conflicts

---
+++ Lab1-2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        inputDevice InputDevice = new inputDevice();
+        OutputDevice outputdevice = new OutputDevice();
+        Application app = new Application(InputDevice, outputdevice);
+    }
+}
